96f5ac6720cabff98791b88178514b0905156234
bsood01 <78615767+bsood01@users.noreply.github.com>
2022-03-17 23:34:19 +0000

    Working shortest path and route

--- FinalProject.java
+++ FinalProject.java
@@ -1,6 +1,10 @@
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.Arrays;
+import java.util.Collections;
 
 
 public class FinalProject {
@@ -49,6 +53,10 @@ public class FinalProject {
 		}*/
 		option1();
 	}
+	
+	/**
+	 * Method for finding shortest path
+	 */
 	private static void option1() {
 		Scanner scan;
 		File input = new File("stops.txt");
@@ -86,11 +94,11 @@ public class FinalProject {
 					//System.out.println(to);
 					//cost associated with stop_times.txt is 1
 					DirectedEdge e = new DirectedEdge(from,to,1);
+					
 					//important so as to not add multiple edge between same vertices
 					if(!graph.contains(e,from)) {
 						graph.addEdge(e);
 					}
-					//save stopID for this line
 					prevStop=Integer.parseInt(split[3]);
 					//System.out.println(prevStop);
 					//System.out.println("");
@@ -98,10 +106,8 @@ public class FinalProject {
 				//else we move on to the next 
 				else {
 					tripID=Integer.parseInt(split[0]);
-					//save stopID for this line
 					prevStop=Integer.parseInt(split[3]);
 				}
-				
 				
 			}
 			for(DirectedEdge e:graph.edges() ) {
@@ -110,6 +116,102 @@ public class FinalProject {
 			System.out.println(graph.E());
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+		
+		input = new File("transfers.txt");
+		try {
+			scan = new Scanner(input);
+			scan.nextLine();
+			
+			//adding tranfers.txt edges to the existing graph
+			while(scan.hasNextLine()) {
+				String in = scan.nextLine();
+				String[] split = in.split(",");
+				int from = map.getIndex(Integer.parseInt(split[0]));
+				int to = map.getIndex(Integer.parseInt(split[1]));
+				
+				//checking the transfer type for the cost
+				double weight =0;
+				if(Integer.parseInt(split[2])==0) {
+					weight=2;
+				}
+				else {
+					weight=Double.parseDouble(split[3])/100;
+				}
+				
+				DirectedEdge e = new DirectedEdge(from,to,weight);
+				if(!graph.contains(e,from)) {
+					graph.addEdge(e);
+				}
+			}
+			for(DirectedEdge e:graph.edges() ) {
+				System.out.println(e.toString());
+			}
+			System.out.println(graph.E());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Graph is complete at this point
+		
+		Scanner inp = new Scanner(System.in);
+		System.out.println("Enter StopId for starting stop (or enter 'quit' to exit): " );
+		int stop1ID=0;
+		int stop2ID=0;
+		boolean quit=false;
+		//get user input for the 2 stops
+		while(true) {
+			if (inp.hasNextInt()) {	
+				int ID =inp.nextInt();
+				if(map.contains(ID)) {
+					if(stop1ID==0) {
+						stop1ID=ID;
+						System.out.println("Enter the 2nd bus stopId. ");
+					}
+					else {
+						stop2ID=ID;
+						break;
+					}
+				}
+				else {
+					System.out.println("Error. Enter a VALID bus stop (or 'quit' to exit): ");
+				}
+			}
+			else if(inp.hasNext(("quit|Quit|q|Q"))){
+				System.out.println("Exitting App.");
+				quit=true;
+				break;
+			}
+			else {
+				System.out.println("Error. Enter a VALID bus stop ID (or 'quit' to exit): ");
+				inp.next();
+			}
+		}
+		if(!quit) {
+			//getting the shortest path
+			Dijkstra d = new Dijkstra();
+			d.shortestPath(map.getIndex(stop1ID), graph);
+			double[] distTo= d.getDistTo();
+			DirectedEdge[] edgeTo=d.getEdgeTo();
+			if(distTo[map.getIndex(stop2ID)]!=-1) {
+				String path=Integer.toString(stop2ID);
+				for(DirectedEdge e = edgeTo[map.getIndex(stop2ID)]; e != null; e = edgeTo[e.from()]) {
+					path = map.getStopID(e.from())+"->"+path;
+				}
+				System.out.println(path+" "+distTo[map.getIndex(stop2ID)]);
+				
+			}
+			else {
+				System.out.printf("No path exists between %d and %d",stop1ID,stop2ID);
+			}
+			
+			/*double min=Integer.MAX_VALUE;
+			for(int i=0;i<distTo.length;i++) {
+				if(distTo[i]<min) {
+					min=distTo[i];
+				}
+			}
+			System.out.println(min);*/
 		}
 		
 	}
